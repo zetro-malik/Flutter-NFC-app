67d80613eadee59b3deff14e0420d05681b19ed3
zetro-malik <zeeshanimsb@gmail.com>
2023-10-27 16:51:30 +0500

    add writelink and contact info write

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'dart:convert';
 import 'dart:typed_data';
 
 import 'package:flutter/material.dart';
@@ -54,9 +55,18 @@ class MyAppState extends State<MyApp> {
                           ),
                             child: Text('Tag Read'), onPressed:() =>  _tagRead(context)),
                        
+                       
+
                         ElevatedButton(
                             child: const Text("Text 'somthing text abc...' Write"),
                             onPressed:() =>  _ndefWriteText(context)),
+                        
+                         ElevatedButton(
+                            child: const Text("Link Write"),
+                            onPressed:() =>  _ndefWriteLink(context)),
+                             ElevatedButton(
+                            child: const Text("Contact info Write"),
+                            onPressed:() =>  _ndefWriteContact(context)),
                         ElevatedButton(
                             child: const Text('Application Launch Write'),
                             onPressed:() =>  _ndefWrite(context)),
@@ -113,6 +123,119 @@ class MyAppState extends State<MyApp> {
   }
 
 
+
+
+  // add link 
+  void _ndefWriteLink(context) {
+    try{
+      NfcManager.instance.stopSession();
+    }catch(e){
+
+    }
+    dialogContent(context);
+    NfcManager.instance.startSession(onDiscovered: (NfcTag tag) async {
+        Navigator.pop(context);
+      var ndef = Ndef.from(tag);
+      if (ndef == null || !ndef.isWritable) {
+        result.value = 'Tag is not ndef writable';
+        NfcManager.instance.stopSession(errorMessage: result.value);
+        return;
+      }
+      NdefMessage message = NdefMessage([
+        NdefRecord.createUri(Uri.parse('https://www.google.com/m')),
+
+      ]);
+
+      try {
+        await ndef.write(message);
+        result.value = 'Success to "Ndef Write"';
+
+
+        Future.delayed(Duration(seconds: 1),() =>  NfcManager.instance.stopSession(errorMessage: result.value.toString()),);
+
+      } catch (e) {
+        result.value = e;
+       Future.delayed(Duration(seconds: 1),() =>  NfcManager.instance.stopSession(errorMessage: result.value.toString()),);
+        return;
+      }
+    });
+  }
+
+
+  void _ndefWriteContact(context) {
+  try {
+    // Stop the current NFC session if there's one
+    NfcManager.instance.stopSession();
+  } catch (e) {
+    // Handle any errors if stopping the session fails
+  }
+
+  // Display a dialog
+  dialogContent(context);
+
+  // Define contact details (replace with your own contact information)
+  String contactName = "John Doe";
+  String contactPhoneNumber = "+1234567890";
+  String contactEmail = "johndoe@example.com";
+
+  // Create a vCard (VCF) string with the contact details
+  String vCardData = "BEGIN:VCARD\n"
+      "VERSION:3.0\n"
+      "FN:$contactName\n"
+      "TEL:$contactPhoneNumber\n"
+      "EMAIL:$contactEmail\n"
+      "END:VCARD";
+
+  // Convert the vCard data to bytes
+  List<int> vCardBytes = utf8.encode(vCardData);
+
+  // Start an NFC session
+  NfcManager.instance.startSession(onDiscovered: (NfcTag tag) async {
+    Navigator.pop(context); // Close the dialog
+
+    var ndef = Ndef.from(tag);
+
+    // Check if the NFC tag is writable
+    if (ndef == null || !ndef.isWritable) {
+      result.value = 'Tag is not NDEF writable';
+      NfcManager.instance.stopSession(errorMessage: result.value);
+      return;
+    }
+
+    // Create an NDEF record with a custom MIME type (vCard) and the contact information
+    NdefRecord mimeRecord = NdefRecord.createMime(
+      'text/vcard', // MIME type for vCard
+
+      Uint8List.fromList(vCardData.codeUnits),
+    );
+
+    // Create an NDEF message containing the vCard record
+    NdefMessage message = NdefMessage([mimeRecord]);
+
+    try {
+      // Write the NDEF message to the tag
+      await ndef.write(message);
+      result.value = 'Success to "NDEF Write"';
+
+      // Delayed session stop to allow time to see the result
+      Future.delayed(Duration(seconds: 1), () {
+        NfcManager.instance.stopSession(errorMessage: result.value.toString());
+      });
+    } catch (e) {
+      result.value = e;
+
+      // Delayed session stop in case of an error
+      Future.delayed(Duration(seconds: 1), () {
+        NfcManager.instance.stopSession(errorMessage: result.value.toString());
+      });
+    }
+  });
+}
+
+
+
+
+
   void _ndefWrite(context) {
     try{
       NfcManager.instance.stopSession();
@@ -128,9 +251,6 @@ class MyAppState extends State<MyApp> {
         NfcManager.instance.stopSession(errorMessage: result.value);
         return;
       }
-      //NdefMessage message = NdefMessage([NdefRecord.createText(abc.text.isEmpty ? "Nothing added" : abc.text )]);
-
-
       NdefMessage message = NdefMessage([
       //  NdefRecord.createText('Zetro Malik'),
         //NdefRecord.createText('App Store:'),
